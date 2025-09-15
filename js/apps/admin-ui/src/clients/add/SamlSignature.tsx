ai-code-review-benchmarks/greptile-keycloak

Documentation and UI changes Closes #17028
Signed-off-by: rmartinc <rmartinc@redhat.com>

js/apps/admin-ui/src/clients/add/SamlSignature.tsx
@@ -1,6 +1,6 @@
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { SelectControl } from "@keycloak/keycloak-ui-shared";
+import { SelectControl, TextControl } from "@keycloak/keycloak-ui-shared";
 import { FormAccess } from "../../components/form/FormAccess";
 import { convertAttributeNameToForm } from "../../util";
 import { FormFields } from "../ClientDetails";
@@ -48,6 +48,13 @@ export const SamlSignature = () => {
   );
   const samlEncryption = watch(
     convertAttributeNameToForm<FormFields>("attributes.saml.encrypt"),
+    "false",
+  );
+
+  const useMetadataDescriptorUrl = watch(
+    convertAttributeNameToForm<FormFields>(
+      "attributes.saml.useMetadataDescriptorUrl",
+    ),
     "false",
   );
 
@@ -100,6 +107,26 @@ export const SamlSignature = () => {
               key: value,
               value: name,
             }))}
+          />
+          <TextControl
+            name={convertAttributeNameToForm<FormFields>(
+              "attributes.saml.metadataDescriptorUrl",
+            )}
+            label={t("spMetadataDescriptorUrl")}
+            labelIcon={t("spMetadataDescriptorUrlHelp")}
+            type="url"
+            rules={{
+              required: {
+                value: useMetadataDescriptorUrl === "true",
+                message: t("required"),
+              },
+            }}
+          />
+          <Toggle
+            name={convertAttributeNameToForm(
+              "attributes.saml.useMetadataDescriptorUrl",
+            )}
+            label="spUseMetadataDescriptorUrl"
           />
           {samlEncryption === "true" && <SamlEncryption />}
         </>

aganga19/graphite-keycloak

js/apps/admin-ui/src/clients/add/SamlSignature.tsx
@@ -1,6 +1,6 @@
 import { useFormContext } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { SelectControl } from "@keycloak/keycloak-ui-shared";
+import { SelectControl, TextControl } from "@keycloak/keycloak-ui-shared";
 import { FormAccess } from "../../components/form/FormAccess";
 import { convertAttributeNameToForm } from "../../util";
 import { FormFields } from "../ClientDetails";
@@ -48,6 +48,13 @@ export const SamlSignature = () => {
   );
   const samlEncryption = watch(
     convertAttributeNameToForm<FormFields>("attributes.saml.encrypt"),
+    "false",
+  );
+
+  const useMetadataDescriptorUrl = watch(
+    convertAttributeNameToForm<FormFields>(
+      "attributes.saml.useMetadataDescriptorUrl",
+    ),
     "false",
   );
 
@@ -100,6 +107,26 @@ export const SamlSignature = () => {
               key: value,
               value: name,
             }))}
+          />
+          <TextControl
+            name={convertAttributeNameToForm<FormFields>(
+              "attributes.saml.metadataDescriptorUrl",
+            )}
+            label={t("samlClientMetadataDescriptorUrl")}
+            labelIcon={t("samlClientMetadataDescriptorUrlHelp")}
+            type="url"
+            rules={{
+              required: {
+                value: useMetadataDescriptorUrl === "true",
+                message: t("required"),
+              },
+            }}
+          />
+          <Toggle
+            name={convertAttributeNameToForm(
+              "attributes.saml.useMetadataDescriptorUrl",
+            )}
+            label="samlClientUseMetadataDescriptorUrl"
           />
           {samlEncryption === "true" && <SamlEncryption />}
         </>