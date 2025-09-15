keycloak/keycloak

Rename RLM to Workflows
Closes #42512

Signed-off-by: vramik <vramik@redhat.com>

model/jpa/src/main/java/org/keycloak/models/workflow/AbstractUserWorkflowProvider.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright 2025 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.models.workflow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
+import org.keycloak.component.ComponentModel;
+import org.keycloak.connections.jpa.JpaConnectionProvider;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.jpa.entities.UserEntity;
+
+public abstract class AbstractUserWorkflowProvider extends EventBasedWorkflowProvider {
+
+    private final EntityManager em;
+
+    public AbstractUserWorkflowProvider(KeycloakSession session, ComponentModel model) {
+        super(session, model);
+        this.em = session.getProvider(JpaConnectionProvider.class).getEntityManager();
+    }
+
+    @Override
+    public List<String> getEligibleResourcesForInitialAction() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<String> query = cb.createQuery(String.class);
+        Root<UserEntity> userRoot = query.from(UserEntity.class);
+        List<Predicate> predicates = new ArrayList<>();
+
+        // Subquery will find if a state record exists for the user and policy
+        // SELECT 1 FROM WorkflowActionStateEntity s WHERE s.resourceId = userRoot.id AND s.workflowId = :workflowId
+        Subquery<Integer> subquery = query.subquery(Integer.class);
+        Root<WorkflowActionStateEntity> stateRoot = subquery.from(WorkflowActionStateEntity.class);
+        subquery.select(cb.literal(1));
+        subquery.where(
+            cb.and(
+                cb.equal(stateRoot.get("resourceId"), userRoot.get("id")),
+                cb.equal(stateRoot.get("workflowId"), getModel().getId())
+            )
+        );
+        Predicate notExistsPredicate = cb.not(cb.exists(subquery));
+        predicates.add(notExistsPredicate);
+
+        predicates.addAll(getConditionsPredicate(cb, query, userRoot));
+
+        query.select(userRoot.get("id")).where(predicates);
+
+        return em.createQuery(query).getResultList();
+    }
+
+    private List<Predicate> getConditionsPredicate(CriteriaBuilder cb, CriteriaQuery<String> query, Root<UserEntity> path) {
+        List<String> conditions = getModel().getConfig().getOrDefault("conditions", List.of());
+
+        if (conditions.isEmpty()) {
+            return List.of();
+        }
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        for (String providerId : conditions) {
+            WorkflowConditionProvider condition = resolveCondition(providerId);
+            Predicate predicate = condition.toPredicate(cb, query, path);
+
+            if (predicate != null) {
+                predicates.add(predicate);
+            }
+        }
+
+        return predicates;
+    }
+
+    @Override
+    public void close() {
+        // no-op
+    }
+
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    protected RealmModel getRealm() {
+        return getSession().getContext().getRealm();
+    }
+}

services/src/main/java/org/keycloak/models/workflow/WorkflowsEventListenerFactory.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright 2025 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.models.workflow;
+
+import java.time.Duration;
+
+import org.keycloak.Config.Scope;
+import org.keycloak.common.Profile;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.EnvironmentDependentProviderFactory;
+import org.keycloak.services.scheduled.ClusterAwareScheduledTaskRunner;
+import org.keycloak.timer.TimerProvider;
+import org.keycloak.provider.ProviderEvent;
+
+public class WorkflowsEventListenerFactory implements EventListenerProviderFactory, EnvironmentDependentProviderFactory {
+
+    public static final String ID = "workflow-event-listener";
+    private static final long DEFAULT_ACTION_RUNNER_TASK_INTERVAL = Duration.ofHours(12).toMillis();
+    private long actionRunnerTaskInterval;
+
+    @Override
+    public EventListenerProvider create(KeycloakSession session) {
+        return new WorkflowEventListener(session);
+    }
+
+    @Override
+    public boolean isGlobal() {
+        return true;
+    }
+
+    @Override
+    public void init(Scope config) {
+        actionRunnerTaskInterval = config.getLong("actionRunnerTaskInterval", DEFAULT_ACTION_RUNNER_TASK_INTERVAL);
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+        factory.register(event -> {
+            KeycloakSession session = event.getKeycloakSession();
+
+            if (session != null) {
+                onEvent(event, session);
+            }
+        });
+        scheduleActionRunnerTask(factory);
+    }
+
+    private void onEvent(ProviderEvent event, KeycloakSession session) {
+        WorkflowEventListener provider = (WorkflowEventListener) session.getProvider(EventListenerProvider.class, getId());
+        provider.onEvent(event);
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public boolean isSupported(Scope config) {
+        return Profile.isFeatureEnabled(Profile.Feature.WORKFLOWS);
+    }
+
+    private void scheduleActionRunnerTask(KeycloakSessionFactory factory) {
+        try (KeycloakSession session = factory.create()) {
+            TimerProvider timer = session.getProvider(TimerProvider.class);
+            timer.schedule(new ClusterAwareScheduledTaskRunner(factory, new ResourceActionRunnerScheduledTask(factory), actionRunnerTaskInterval), actionRunnerTaskInterval);
+        }
+    }
+}

aganga19/graphite-keycloak

model/jpa/src/main/java/org/keycloak/models/workflow/AbstractUserWorkflowProvider.java
@@ -1,0 +1,105 @@
+/*
+ * Copyright 2025 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.models.workflow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Subquery;
+import org.keycloak.component.ComponentModel;
+import org.keycloak.connections.jpa.JpaConnectionProvider;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.jpa.entities.UserEntity;
+
+public abstract class AbstractUserWorkflowProvider extends EventBasedWorkflowProvider {
+
+    private final EntityManager em;
+
+    public AbstractUserWorkflowProvider(KeycloakSession session, ComponentModel model) {
+        super(session, model);
+        this.em = session.getProvider(JpaConnectionProvider.class).getEntityManager();
+    }
+
+    @Override
+    public List<String> getEligibleResourcesForInitialStep() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<String> query = cb.createQuery(String.class);
+        Root<UserEntity> userRoot = query.from(UserEntity.class);
+        List<Predicate> predicates = new ArrayList<>();
+
+        // Subquery will find if a state record exists for the user and policy
+        // SELECT 1 FROM WorkflowActionStateEntity s WHERE s.resourceId = userRoot.id AND s.workflowId = :workflowId
+        Subquery<Integer> subquery = query.subquery(Integer.class);
+        Root<WorkflowStateEntity> stateRoot = subquery.from(WorkflowStateEntity.class);
+        subquery.select(cb.literal(1));
+        subquery.where(
+            cb.and(
+                cb.equal(stateRoot.get("resourceId"), userRoot.get("id")),
+                cb.equal(stateRoot.get("workflowId"), getModel().getId())
+            )
+        );
+        Predicate notExistsPredicate = cb.not(cb.exists(subquery));
+        predicates.add(notExistsPredicate);
+
+        predicates.addAll(getConditionsPredicate(cb, query, userRoot));
+
+        query.select(userRoot.get("id")).where(predicates);
+
+        return em.createQuery(query).getResultList();
+    }
+
+    private List<Predicate> getConditionsPredicate(CriteriaBuilder cb, CriteriaQuery<String> query, Root<UserEntity> path) {
+        List<String> conditions = getModel().getConfig().getOrDefault("conditions", List.of());
+
+        if (conditions.isEmpty()) {
+            return List.of();
+        }
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        for (String providerId : conditions) {
+            WorkflowConditionProvider condition = resolveCondition(providerId);
+            Predicate predicate = condition.toPredicate(cb, query, path);
+
+            if (predicate != null) {
+                predicates.add(predicate);
+            }
+        }
+
+        return predicates;
+    }
+
+    @Override
+    public void close() {
+        // no-op
+    }
+
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    protected RealmModel getRealm() {
+        return getSession().getContext().getRealm();
+    }
+}

services/src/main/java/org/keycloak/models/workflow/WorkflowsEventListenerFactory.java
@@ -1,0 +1,91 @@
+/*
+ * Copyright 2025 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.models.workflow;
+
+import java.time.Duration;
+
+import org.keycloak.Config.Scope;
+import org.keycloak.common.Profile;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.EnvironmentDependentProviderFactory;
+import org.keycloak.services.scheduled.ClusterAwareScheduledTaskRunner;
+import org.keycloak.timer.TimerProvider;
+import org.keycloak.provider.ProviderEvent;
+
+public class WorkflowsEventListenerFactory implements EventListenerProviderFactory, EnvironmentDependentProviderFactory {
+
+    public static final String ID = "workflow-event-listener";
+    private static final long DEFAULT_STEP_RUNNER_TASK_INTERVAL = Duration.ofHours(12).toMillis();
+    private long stepRunnerTaskInterval;
+
+    @Override
+    public EventListenerProvider create(KeycloakSession session) {
+        return new WorkflowEventListener(session);
+    }
+
+    @Override
+    public boolean isGlobal() {
+        return true;
+    }
+
+    @Override
+    public void init(Scope config) {
+        stepRunnerTaskInterval = config.getLong("stepRunnerTaskInterval", DEFAULT_STEP_RUNNER_TASK_INTERVAL);
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+        factory.register(event -> {
+            KeycloakSession session = event.getKeycloakSession();
+
+            if (session != null) {
+                onEvent(event, session);
+            }
+        });
+        scheduleStepRunnerTask(factory);
+    }
+
+    private void onEvent(ProviderEvent event, KeycloakSession session) {
+        WorkflowEventListener provider = (WorkflowEventListener) session.getProvider(EventListenerProvider.class, getId());
+        provider.onEvent(event);
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public boolean isSupported(Scope config) {
+        return Profile.isFeatureEnabled(Profile.Feature.WORKFLOWS);
+    }
+
+    private void scheduleStepRunnerTask(KeycloakSessionFactory factory) {
+        try (KeycloakSession session = factory.create()) {
+            TimerProvider timer = session.getProvider(TimerProvider.class);
+            timer.schedule(new ClusterAwareScheduledTaskRunner(factory, new ResourceStepRunnerScheduledTask(factory), stepRunnerTaskInterval), stepRunnerTaskInterval);
+        }
+    }
+}